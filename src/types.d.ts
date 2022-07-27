0eed640297e3d8c2f7eabe16afdd925de2a8aa3d
TJ Grist <tjgrst@gmail.com>
2022-07-27 11:05:22 -0700

    add auro-datepicker

---
+++ src/types.d.ts
@@ -0,0 +1,28 @@
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import React from "react";
+
+declare global {
+  namespace JSX {
+    interface IntrinsicElements {
+      "auro-header": any;
+      "auro-button": any;
+      "auro-button-light": any;
+      "auro-checkbox-group": any;
+      "auro-checkbox": any;
+      "auro-radio-group": any;
+      "auro-radio": any;
+      "auro-alert": any;
+      "auro-icon": any;
+      "auro-flight": any;
+      "auro-flightline": any;
+      "auro-input": any;
+      "auro-dropdown": any;
+      "auro-menu": any;
+      "auro-menuoption": any;
+      "auro-skeleton": any;
+      "auro-drawer": any;
+      "auro-hyperlink": any;
+      "auro-datepicker": any;
+    }
+  }
+}
